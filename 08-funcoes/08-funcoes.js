45844a7a9ab29d12e982f6107c4f8d604cbfe8b4
CurtyRaissa <raissacurty@gmail.com>
2023-02-01 22:50:22 -0300

    solução da funcão 2

---
+++ 08-funcoes/08-funcoes.js
@@ -0,0 +1,12 @@
+// 1. Crie uma função que recebe um array de numeros e retorna um novo array com o último número dividido por 2
+
+
+
+
+// 2. Crie uma função que pede o nome do usuário em um prompt e executa um alert dando as boas - vindas ao site
+
+function pedirNome(){
+  let nome = prompt("Qual é o seu nome?")
+  alert(`Boas-Vindas ao site ${nome}`)
+}
+pedirNome()
