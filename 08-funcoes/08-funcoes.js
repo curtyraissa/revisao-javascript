28274906b6718fa5c4f4f328a319afb737c41db3
CurtyRaissa <raissacurty@gmail.com>
2023-02-02 21:15:05 -0300

    solução funções

--- 08-funcoes/08-funcoes.js
+++ 08-funcoes/08-funcoes.js
@@ -1,5 +1,10 @@
 // 1. Crie uma função que recebe um array de numeros e retorna um novo array com o último número dividido por 2
-
+divideArray([1, 2, 4])
+  function divideArray(arr){
+  let x = arr.length - 1
+  arr[x] = (arr[x]) / 2
+  console.log(arr)
+}
 
 
 
--- 10-lacos/10-lacos.js
+++ 10-lacos/10-lacos.js
@@ -1,4 +1,17 @@
 // 1. Faça uma função que recebe um array de números positivos e retorna qual é o menor número dele
+retornaArray([5, 2, 3, 4, 5, 6, 0])
+function retornaArray(arr){
+    let menor = arr[0]
+    for(let i=0; i<arr.length; i++){
+        if(menor>arr[i]){
+            menor = arr[i]
+        }
+    }
+console.log(menor)
+}
+
+
+
 // 2. Dado o objeto abaixo, imprima no console a lista com os jogos favoritos da professora
     
 //     ```jsx
@@ -8,6 +21,7 @@
 //         jogosPreferidos: ["Chrono Trigger", "Undertale", "Hollow Knight", "Stray"] 
 //     }
     
+
 //     // --------- Formato que a lista deve ser impressa ---------//
 //     Jogos favoritos da Professora Let:
 //     1) Chrono Trigger
