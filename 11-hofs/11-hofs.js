828cd4a0a278e4e81cbbd86b5f84744db8b1c0a5
CurtyRaissa <raissacurty@gmail.com>
2023-02-03 23:42:27 -0300

    solução DOM 1

--- 11-hofs/11-hofs.js
+++ 11-hofs/11-hofs.js
@@ -1,27 +1,27 @@
-// 1. Usando HOFs, crie uma função que recebe um array e retorna apenas os números pares da lista original, mas multiplicados por 2
+// // 1. Usando HOFs, crie uma função que recebe um array e retorna apenas os números pares da lista original, mas multiplicados por 2
 
-// const lista = [1, 2, 3, 4, 5, 6]
-// const novaLista1 = lista.filter((num) => num % 2 == 0)
-// const novaLista2 = novaLista1.map((num) => num * 2)
-// console.log(novaLista2)
+const lista = [1, 2, 3, 4, 5, 6]
+const novaLista1 = lista.filter((num) => num % 2 == 0)
+const novaLista2 = novaLista1.map((num) => num * 2)
+console.log(novaLista2)
 
-//         2. Abaixo temos um array de produtos de uma floricultura e serão pedidas algumas operações:
-
-
-const produtos = [
-    { nome: "Margaridas", categoria: "Flor", preco: 10.00 },
-    { nome: "Vaso", categoria: "Utilidades", preco: 25.00 },
-    { nome: "Regador", categoria: "Utilidades", preco: 30.00 },
-    { nome: "Rosas", categoria: "Flor", preco: 20.00 },
-    { nome: "Tesoura", categoria: "Utilidades", preco: 10.00 },
-    { nome: "Orquídeas", categoria: "Flor", preco: 30.00 },
-]
+// //         2. Abaixo temos um array de produtos de uma floricultura e serão pedidas algumas operações:
 
 
-//             1. Utilizando HOFs, crie um novo array modificando os preços dos produtos para dar 10% de desconto em relação ao preço original
-//             2. Utilizando HOFs, crie um array contendo somente os produtos da categoria “Utilidades”
-//             3. Utilizando HOFs, atribua um desconto de 30% nos produtos com a categoria “Flores”
-//             4. Utilizando HOFs, crie um array que possua os nomes das flores disponíveis ⇒ `[”Margaridas”, “Rosas”, “Orquídeas”]`
+// const produtos = [
+//     { nome: "Margaridas", categoria: "Flor", preco: 10.00 },
+//     { nome: "Vaso", categoria: "Utilidades", preco: 25.00 },
+//     { nome: "Regador", categoria: "Utilidades", preco: 30.00 },
+//     { nome: "Rosas", categoria: "Flor", preco: 20.00 },
+//     { nome: "Tesoura", categoria: "Utilidades", preco: 10.00 },
+//     { nome: "Orquídeas", categoria: "Flor", preco: 30.00 },
+// ]
+
+
+// //             1. Utilizando HOFs, crie um novo array modificando os preços dos produtos para dar 10% de desconto em relação ao preço original
+// //             2. Utilizando HOFs, crie um array contendo somente os produtos da categoria “Utilidades”
+// //             3. Utilizando HOFs, atribua um desconto de 30% nos produtos com a categoria “Flores”
+// //             4. Utilizando HOFs, crie um array que possua os nomes das flores disponíveis ⇒ `[”Margaridas”, “Rosas”, “Orquídeas”]`
 
 const novo1 = produtos.map((num) => (num.preco - num.preco * 0.1))
 console.log(novo1)
@@ -36,3 +36,4 @@ console.log(novo33)
 const novo4 = produtos.filter((item)=>item.categoria == "Flor")
 const novo44 = novo4.filter((item) => item.nome)
 console.log(novo44)
+
--- 12-manipulacao-do-dom-1/12-manipulacao-do-dom-1.js
+++ 12-manipulacao-do-dom-1/12-manipulacao-do-dom-1.js
@@ -1,4 +1,32 @@
 // 1. Crie uma tela com um botão. Ao apertar o botão, deve aparecer um prompt para o usuário digitar sua cor favorita entre as opções: vermelho, verde ou azul. Mostre o nome da cor em um h1 na tela. Esse h1 deve estar escrito na cor escolhida.
+
+function escolherCor() {
+    const cor = prompt("qual é a sua cor favorita entre vermelho, verde ou azul?")
+    const alterar = document.querySelector(".botaozinho h1")
+    alterar.innerHTML = cor
+    if (cor == "vermelho") {
+        alterar.style.color = 'red'
+    } else if (cor == "azul") {
+        alterar.style.color = 'blue'
+    } else {
+        alterar.style.color = 'green'
+    }
+}
+
+
 // 2. Crie uma página com um input e um botão. A pessoa pode inserir o seu nome no input e, ao apertar o botão:
 //     1. O nome irá aparecer em um h1 na tela, embaixo do input e do botão
 //     2. O input será limpado para poder receber novos textos
+
+
+
+const btn = document.querySelector("#btn")
+btn.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const nomeEscrito = nome.value
+
+    const novo = document.querySelector("#novo")
+
+    novo.innerHTML = `<h1> ${nomeEscrito} </h1>`
+})
