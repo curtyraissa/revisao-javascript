bc34cb38d1a104fcb64b92e923a8ac38a984ebfb
CurtyRaissa <raissacurty@gmail.com>
2023-02-04 16:23:31 -0300

    solução axxios

--- 14-axios/14-axios.js
+++ 14-axios/14-axios.js
@@ -1,2 +1,35 @@
 // 1. Mostre uma lista com o nome dos 20 primeiros pokémons da PokéAPI: [https://pokeapi.co/](https://pokeapi.co/)
+
+const lista = document.querySelector(".lista")
+
+function getAPI() {
+    const promise = axios.get(" https://pokeapi.co/api/v2/pokemon")
+    promise.then((res) => {
+        for (let i = 0; i < 20; i++) {
+            lista.innerHTML += `<li> ${res.data.results[i].name} </li>`
+        }
+
+    })
+    promise.catch((err) => {
+        console.log(err)
+    })
+}
+getAPI()
+
 // 2. Mostre uma lista com os alunos que são estudantes da Lufa Lufa (hufflepuff) a API do Harry Potter: [https://hp-api.onrender.com/](https://hp-api.onrender.com/)
+
+const lista = document.querySelector(".lista")
+
+function getAPI() {
+    const promise = axios.get("https://hp-api.onrender.com/api/characters/house/hufflepuff")
+    promise.then((res) => {
+        console.log(res)
+        for (let i = 0; i < 18; i++) {
+            lista.innerHTML += `<li> ${res.data[i].name}</li>`
+        }
+    })
+    promise.catch((err) => {
+        console.log(err)
+    })
+}
+getAPI()
